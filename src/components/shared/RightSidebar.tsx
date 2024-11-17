705a0c24bdf137806b3c14b19c5bce15557a2bdc
Kushal Kumar <kushalmalviya1302@gmail.com>
2024-11-17 23:39:05 +0530

    feat: added bottom and right side bar and worked on onboarding page

--- src/components/shared/RightSidebar.tsx
+++ src/components/shared/RightSidebar.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const RightSidebar = () => {
   return (
-    <div>
-      RightSidebar
-    </div>
-  )
-}
+    <section className="rightsidebar custom-scrollbar">
+      <div className="flex flex-1 justify-start flex-col">
+        <h3 className="text-heading4-medium text-light-1">
+          Suggested Communities
+        </h3>
+      </div>
+      <div className="flex flex-1 justify-start flex-col">
+        <h3 className="text-heading4-medium text-light-1">Suggested Users</h3>
+      </div>
+    </section>
+  );
+};
 
-export default RightSidebar
+export default RightSidebar;
